150125fca56d9a5cac466d004cd946586709ff5b
Ghansham Irashetti <105876844+Ghanasham2004@users.noreply.github.com>
2023-08-12 17:06:27 +0530

    React Router Tut-5 Active Links

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { NavLink } from "react-router-dom"; // NavLink is used to create links in the navbar that are active when clicked on and inactive on other pages.
+const Navbar = () => {
+  // The navLinkStyle function is used to style the active and inactive links in the navbar. The isActive property is passed to the function by the NavLink component.
+  //   const navLinkStyle = ({ isActive }) => {
+  //     return {
+  //       color: isActive ? "red" : "blue",
+  //       fontWeight: isActive ? "bold" : "normal",
+  //     };
+  //   };
+  return (
+    <nav>
+      <ul>
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+        <li>
+          <NavLink to="/about">About</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
