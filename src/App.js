f743358330b8e125fc22f3375fcac47122b84d79
Ghansham Irashetti <105876844+Ghanasham2004@users.noreply.github.com>
2023-08-12 18:44:04 +0530

    React Router Tut - 9 - Index Route

--- src/App.js
+++ src/App.js
@@ -17,6 +17,8 @@ function App() {
       <Route path="/about" element={<About />} />
       <Route path="/order-summary" element={<OrderSummary/>} />
       <Route path="/products" element={<Products/>} >
+        {/* add index route for when user render products route then we show feature content first */}
+        <Route index element={<FeaturedProducts/>} />
         <Route path="feature" element={<FeaturedProducts/>} />
         <Route path="new" element={<NewProducts/>} />
       </Route>
